7615e4c998f5fce03aa888e5d82d0ecad1e155d8
Alexander <webisforce@gmail.com>
2021-03-02 13:18:11 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"603e10ab627982001eb712fd",
+				"603e10ab627982001eb712ff"
+			]
+		},
+		"603e10ab627982001eb712fd": {
+			"id": "603e10ab627982001eb712fd",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"603e10ab627982001eb712ff": {
+			"id": "603e10ab627982001eb712ff",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as Sadsad } from "./Sadsad"
